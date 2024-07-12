2fed2d7fde1f29de03e683a20b2f712eaf9dd82f
yagod <yagomartins30@gmail.com>
2024-07-11 22:03:29 -0400

    codigo organizado, boss funcionado porem sem colisao

--- entities.py
+++ entities.py
@@ -1,5 +1,4 @@
-import pygame
-import random
+import pygame, random, time
 
 display_W = 800
 display_H = 600
@@ -52,7 +51,7 @@ heart_pattern = [
     "   01   "
 ]
 
-buff_pattern = [
+test_pattern = [
     "111111",
     "111111",
     "111111",
@@ -62,65 +61,170 @@ buff_pattern = [
 ]
 
 boss_pattern = [
-    "            3333            "
-    "           333333           ",
-    "         3233333333         ",
-    "        322333333333        ",
-    "   4443333333333333333444   ",
-    "4444454555333333335554544444",
-    "4444444444555555554444444444",
-    "4444444544444444444454444444",
-    "  444444444445444444444444  ",
-    "    44444444444444444444    ",
-    "         4444444444         "
-]
-background_pattern = [
-    "000111000",
-    "001222100",
-    "012333210",
-    "123444321",
-    "123444321",
-    "012333210",
-    "001222100",
-    "000111000"
+
+    "           3366cc           ",
+    "        32266ccdddee        ",
+    "       32366ccdddeeee       ",
+    "   54433366ccdddeeeeee445   ",
+    "baa54444446ccddeee4444445aab",
+    "bbbff555554444444455555ffbbb",
+    "bbbbaa5555555555555555aabbbb",
+    "  bbbbaff5555555555ffabbbb  ",
+    "    bbbbaaaaaffaaaaabbbb    ",
+    "         bbbbbbbbbb         "
 ]
 
+laser_patternPT1 = [
+ "    eedd22ddee    ",
+ "    eedd22ddee    ",
+ "    eedd22ddee    ",
+ "    eedd22ddee    ",
+ "    eedd22ddee    ",
+ "    eedd22ddee    ",
+ "    eedd22ddee    ",
+ "    eedd22ddee    ",
+ "    eedd22ddee    ",
+ "    eedd22ddee    ",
+]
+
+laser_patternPT2 = [
+ "   eedd2222ddee   ",
+ "e  eedd2222ddee  e",
+ "eeeddd222222dddeee",
+ "eeeddd222222dddeee",
+]
+
+bullet_pattern = [
+    "      e      ",
+    "     ede     ",
+    "     ede     ",
+    "    eddde    ",
+    "    ed2de    ",
+    "   edd2dde   ",
+    "   ed222de   ",
+    "  edd222dde  ",
+    "  edd222dde  ",
+    "eedd22222ddee",
+    "eedd22222ddee",
+    " edd22222dde ",
+    "  edd222dde  ",
+    "  eddd2ddde  ",
+    "   edd2dde   ",
+    "    eddde    ",
+    "     eee"
+
+]
+
+bullet_pattern1 = [
+    "      1      ",
+    "     1g1     ",
+    "     1g1     ",
+    "    1ggg1    ",
+    "    1g8g1    ",
+    "   1gg8gg1   ",
+    "   1g888g1   ",
+    "  1gg888gg1  ",
+    "  1gg888gg1  ",
+    "11gg88888gg11",
+    "11gg88888gg11",
+    " 1gg88888gg1 ",
+    "  1gg888gg1  ",
+    "  1ggg8ggg1  ",
+    "   1gg8gg1   ",
+    "    1ggg1    ",
+    "     111"
+
+]
+
+angry_pattern = [
+"  000         000        ",
+" 01110       01110       ",
+"   011110   0110         ",
+"     01110  01110        ",
+"     01110  0 01110      ",
+"    01110  010  01110    ",
+"  011110  01110   011110 ",
+"01110   011100110    0110",
+" 000  01110   01110   00 ",
+"    01110        0110    ",
+"     000          00     ",
+]
+
+exclamation_pattern = [
+"  000000    ",
+" 01111110   ",
+" 01111110   ",
+" 01111110   ",
+" 01111110   ",
+"  01111110  ",
+"   01111110 ",
+"   01111110 ",
+"     0000   ",
+"    011110  ",
+"   01111110 ",
+"    011110  ",
+"     0000   "
+]
 def color(op):
     colors = {
-        'white': (255, 255, 255),
+        'white': (235, 235, 235),
         'yellow': (255, 255, 102),
         'dyellow': (214, 153, 0),
         'black': (0, 0, 0),
         'red': (255, 50, 50),
+        'lred': (255, 107, 25),
         'dred': (198, 0, 0),
-        'blue': (173, 216, 230),
+        'blue': (133, 209, 255),
+        'dblue': (134, 199, 255),
+        'dblue1': (135, 189, 255),
+        'dblue2': (136, 179, 255),
+        'dblue3': (134, 169, 255),
         'gray': (128, 128, 128),
-        'dgray': (108, 108, 108)
+        'dgray': (108, 108, 108),
+        'dgray1': (98, 98, 98),
+        'dgray2': (88, 88, 88),
+        'dgray3': (87, 78, 88),
     }
     return colors.get(op)
+
 
 def draw_pixel_art(pattern, x, y, ps, display):
     for row_index, row in enumerate(pattern):
         for col_index, pixel in enumerate(row):
+            if pixel == '0':
+                pygame.draw.rect(display, color('black'), (x + col_index * ps, y + row_index * ps, ps, ps))
             if pixel == '1':
                 pygame.draw.rect(display, color('red'), (x + col_index * ps, y + row_index * ps, ps, ps))
             if pixel == '2':
                 pygame.draw.rect(display, color('white'), (x + col_index * ps, y + row_index * ps, ps, ps))
             if pixel == '3':
                 pygame.draw.rect(display, color('blue'), (x + col_index * ps, y + row_index * ps, ps, ps))
+            if pixel == '6':
+                pygame.draw.rect(display, color('dblue'), (x + col_index * ps, y + row_index * ps, ps, ps))
             if pixel == '4':
                 pygame.draw.rect(display, color('gray'), (x + col_index * ps, y + row_index * ps, ps, ps))
             if pixel == '5':
                 pygame.draw.rect(display, color('dgray'), (x + col_index * ps, y + row_index * ps, ps, ps))
-            if pixel == '6':
-                pygame.draw.rect(display, color('black'), (x + col_index * ps, y + row_index * ps, ps, ps))
             if pixel == '7':
                 pygame.draw.rect(display, color('dred'), (x + col_index * ps, y + row_index * ps, ps, ps))
             if pixel == '8':
                 pygame.draw.rect(display, color('yellow'), (x + col_index * ps, y + row_index * ps, ps, ps))
             if pixel == '9':
                 pygame.draw.rect(display, color('dyellow'), (x + col_index * ps, y + row_index * ps, ps, ps))
-
+            if pixel == 'a':
+                pygame.draw.rect(display, color('dgray1'), (x + col_index * ps, y + row_index * ps, ps, ps))
+            if pixel == 'b':
+                pygame.draw.rect(display, color('dgray2'), (x + col_index * ps, y + row_index * ps, ps, ps))
+            if pixel == 'c':
+                pygame.draw.rect(display, color('dblue1'), (x + col_index * ps, y + row_index * ps, ps, ps))
+            if pixel == 'd':
+                pygame.draw.rect(display, color('dblue2'), (x + col_index * ps, y + row_index * ps, ps, ps))
+            if pixel == 'e':
+                pygame.draw.rect(display, color('dblue3'), (x + col_index * ps, y + row_index * ps, ps, ps))
+            if pixel == 'f':
+                pygame.draw.rect(display, color('dgray3'), (x + col_index * ps, y + row_index * ps, ps, ps))
+            if pixel == 'g':
+                pygame.draw.rect(display, color('lred'), (x + col_index * ps, y + row_index * ps, ps, ps))
 
 class Player:
     def __init__(self):
@@ -130,9 +234,12 @@ class Player:
         self.pattern = player_pattern
         self.speed = 6
         self.buffs = []
-        self.life = 3
+        self.life = 5
+        self.max_life = 10
         self.invulnerable = False
         self.doubleShoot = False
+        self.ammo = []
+        self.score = 0
 
     def draw(self, display):
         draw_pixel_art(self.pattern, self.x, self.y, 4, display)
@@ -153,6 +260,12 @@ class Player:
             if not buff.update(self):
                 self.buffs.remove(buff)
 
+    def shoot(self, display):
+        for bullet in self.ammo:
+            bullet[1] -= 10
+            pygame.draw.circle(display, color('white'), (bullet[0] + 14, bullet[1]), 3)
+            if bullet[1] < 0:
+                self.ammo.remove(bullet)
 
 class Enemy:
     def __init__(self, score):
@@ -190,18 +303,93 @@ class Boss:
         self.x = 250
         self.y = 30 
         self.life = 200
-        self.width = 15
+        self.width = 12
         self.speed = 2
-
-    def move(self):
-        self.y += self.speed
-        if self.y > display_H:
-            pass
+        self.ammo = []
+        self.delay = 0
+        self.bullet_speed = 0
+        self.shooting = True
+        self.minigun = False
+        self.cannon = False
+        self.laser = False
+        self.stop = False
+        self.change_direction = False
 
     def draw(self, display):
         draw_pixel_art(boss_pattern, self.x, self.y, self.width, display)
-        
 
+    def move(self):
+        if not self.stop:
+            if self.x < display_W - 338 and not self.change_direction:
+                self.x += self.speed
+                if self.x == display_W -338:
+                    self.change_direction = True
+
+            elif self.change_direction:
+                self.x -= self.speed
+                if self.x == 0:
+                    self.change_direction = False
+
+    def draw_shoot(self, bullet_sz, pattern, display, player):
+        for bullet in self.ammo:
+            bullet[1] += self.bullet_speed
+            draw_pixel_art(pattern, bullet[0], bullet[1], bullet_sz, display)
+            if self.check_collision(player, bullet[0], bullet[1] , bullet_sz + 10, bullet_sz+ 10):
+                player.life -= 1
+                self.ammo.remove(bullet)
+            if bullet[1] > display_H:
+                self.ammo.remove(bullet)
+
+
+    def shoot(self, display, player):
+        if self.laser:
+            if (self.delay // 20) % 2 == 0 and self.x != 250:
+                draw_pixel_art(exclamation_pattern, self.x + 240, self.y - 10, 5, display)
+            self.delay += 1
+            if self.x < 250:
+                self.x += self.speed
+            elif self.x > 250:
+                self.x -= self.speed
+            self.stop = True
+            if self.x == 250:
+                for i in range(12):
+                    draw_pixel_art(laser_patternPT1, self.x + 124, (self.y + 120) + (50 * i), 5, display)
+                draw_pixel_art(laser_patternPT2, self.x + 124, self.y + 550, 5, display)
+                if (self.delay // 20) % 2 == 0:
+                    draw_pixel_art(angry_pattern, self.x + 210, self.y, 4, display)
+                if self.delay % 300 == 0:
+                    self.laser = False
+                    self.shooting = True
+                    self.delay = 0
+                    self.stop = False
+
+        if self.shooting:
+            self.bullet_speed = 3
+            self.delay += 1
+            if self.delay % 30 == 0:
+                self.ammo.append([self.x + self.width + random.randint(80, 200), self.y + 120])
+            self.draw_shoot(4,bullet_pattern, display, player)
+            if self.delay % 300 == 0:
+                self.shooting = False
+                self.minigun = True
+                self.delay = 0
+            
+        if self.minigun:
+            self.bullet_speed = 10
+            self.delay += 1
+            if self.delay % 17 == 0:
+                self.ammo.append([self.x + self.width + random.randint(100, 220), self.y + 120])
+            self.draw_shoot(2, bullet_pattern1, display, player)
+            if self.delay % 300 == 0:
+                self.minigun = False
+                self.laser = True
+                self.delay = 0
+
+    def check_collision(self, player, x, y , sz1, sz2):
+        bullet_rect = pygame.Rect(x, y, sz1, sz2)
+        player_rect = pygame.Rect(player.x, player.y, player.size, player.size)
+        return bullet_rect.colliderect(player_rect)
+    
 class Buff:
     def __init__(self, duration, effect, x, y, pattern):
         self.duration = duration
@@ -224,7 +412,7 @@ class Buff:
         if self.effect == "speed":
             player.speed += 10
         elif self.effect == "heal":
-            if player.life < 5:
+            if player.life < player.max_life:
                 player.life += 1
         elif self.effect == "invulnerability":
             player.invulnerable = True
@@ -254,3 +442,65 @@ class Buff:
         buff_rect = pygame.Rect(self.x, self.y, 24, 24)
         player_rect = pygame.Rect(player.x, player.y, player.size, player.size)
         return buff_rect.colliderect(player_rect)
+    
+
+class Game:
+    def __init__(self, score):
+        self.enemies = [Enemy(score)]
+        self.buffs = []
+        self.boss_fight = False
+
+    def spawn_enemies(self, player):
+        if len(self.enemies) < player.score // 50 + 1:
+            self.enemies.append(Enemy(player.score))
+
+    def create_buff(self, enemy):
+        n = random.randint(0, 20)
+        if n <= 10:
+            return Buff(1, "heal", enemy[0], enemy[1], heart_pattern)
+        if n > 10:
+            return Buff(90, "speed", enemy[0], enemy[1], speed_pattern)
+        return None
+    
+    def checkEnemyCollision(self, bullet, enemy_pos):
+        bullet_rect = pygame.Rect(bullet[0] - 22, bullet[1] - 20, 5, 5)
+        hitbox_size = 45
+        enemy_rect = pygame.Rect(enemy_pos[0] - hitbox_size // 2, enemy_pos[1] - hitbox_size // 2, hitbox_size, hitbox_size)
+        return bullet_rect.colliderect(enemy_rect)
+    
+    def spawn_enemy(self, player, boss, display):
+        for enemy in self.enemies:
+            enemy.draw(display)
+            enemy.move()
+            enemy.outOfScreen(player)
+            for bullet in player.ammo:
+                for enemy in self.enemies:
+                    if self.checkEnemyCollision(bullet, (enemy.x, enemy.y)):
+                        player.score += 10
+                        player.ammo.remove(bullet)
+                        buff = self.create_buff((enemy.x, enemy.y))
+                        if buff:
+                            self.buffs.append(buff)
+                        self.boss_round(player)
+                        enemy.reset_position()
+                        enemy.update_speed(player.score)
+    
+    def boss_round(self, player):
+        if len(self.enemies) < player.score // 50 + 1:
+            self.boss_fight = True
+
+    def spawn_boss(self, boss, player, display):
+        boss.draw(display)
+        boss.shoot(display, player)
+        if not boss.stop:
+            boss.move()
+    
+    def apply_buffs(self, player, display):
+        for buff in self.buffs:
+            buff.draw(display, 3)
+            buff.move()
+            if buff.check_collision(player):
+                player.apply_buff(buff)
+                self.buffs.remove(buff)
+
+    
--- game.py
+++ game.py
@@ -1,5 +1,5 @@
-import pygame, random, time as t
-from entities import Player, Enemy, Buff, Boss, draw_pixel_art, color, heart_pattern, speed_pattern
+import pygame, time as t
+from entities import Player, Boss, Game, draw_pixel_art, color, heart_pattern, speed_pattern
 
 pygame.init()
 
@@ -12,46 +12,19 @@ clock = pygame.time.Clock()
 font_style = pygame.font.SysFont("bahnschrift", 22)
 
 # Variáveis globais
-
-shooting = False
-ammo = []
-score = 0
 player = Player()
-enemies = [Enemy(score)]
-buffs = []
-boss_fight = False
+game = Game(player.score)
 boss = Boss()
 
 def draw_hearts(times, display):
     for i in range(times):
         draw_pixel_art(heart_pattern, 800 * i - (770 * i), 5, 3, display)
 
-def shoot(ammo, display):
-    for bullet in ammo:
-        bullet[1] -= 10
-        pygame.draw.circle(display, color('white'), (bullet[0] + 14, bullet[1]), 3)
-        if bullet[1] < 0:
-            ammo.remove(bullet)
-
 def cleanScreen(display):
     display.fill(color('black'))
 
 def updateScreen():
     pygame.display.update()
-
-def create_buff(x, y):
-    n = random.randint(0, 20)
-    if n <= 10:
-        return Buff(1, "heal", x, y, heart_pattern)
-    if n > 10:
-        return Buff(90, "speed", x, y, speed_pattern)
-    return None
-
-def checkEnemyCollision(bullet, enemy_pos):
-    bullet_rect = pygame.Rect(bullet[0] - 22, bullet[1] - 20, 5, 5)
-    hitbox_size = 45
-    enemy_rect = pygame.Rect(enemy_pos[0] - hitbox_size // 2, enemy_pos[1] - hitbox_size // 2, hitbox_size, hitbox_size)
-    return bullet_rect.colliderect(enemy_rect) 
 
 def message(msg, color, x, y, display, font_style):
     mesg = font_style.render(msg, True, color)
@@ -60,47 +33,7 @@ def message(msg, color, x, y, display, font_style):
 def lose(player):
     return player.life <= 0
 
-def spawn_enemies():
-        if len(enemies) < score // 50 + 1:
-            enemies.append(Enemy(score))
-
-def spawn_enemy():
-    global score 
-    for enemy in enemies:
-        enemy.draw(display)
-        enemy.move()
-        enemy.outOfScreen(player)
-        for bullet in ammo:
-            for enemy in enemies:
-                if checkEnemyCollision(bullet, (enemy.x, enemy.y)):
-                    score += 50
-                    ammo.remove(bullet)
-                    buff = create_buff(enemy.x, enemy.y)
-                    if buff:
-                        buffs.append(buff)
-                    boss_round()
-                    enemy.reset_position()
-                    enemy.update_speed(score)
-
-def boss_round():
-    global boss_fight
-
-    if len(enemies) < score // 50 + 1:
-        boss_fight = True
-
-def spawn_boss():
-    boss.draw(display)
-
-def apply_buffs():
-    for buff in buffs:
-        buff.draw(display, 3)
-        buff.move()
-        if buff.check_collision(player):
-            player.apply_buff(buff)
-            buffs.remove(buff)
-
-def game():  
-    global boss_fight
+def main():  
     alive = True
 
     while alive:
@@ -111,11 +44,10 @@ def game():
             #chamar tela final
 
         draw_hearts(player.life, display)
-        message(f"Score: {score}", color('white'), 685, 3, display, font_style)
+        message(f"Score: {player.score}", color('white'), 685, 3, display, font_style)
         player.draw(display)
-        shoot(ammo, display)
-
-        apply_buffs()
+        player.shoot(display)
+        game.apply_buffs(player, display)
   
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -124,19 +56,19 @@ def game():
                 if event.key == pygame.K_ESCAPE:
                     alive = False
                 if event.key == pygame.K_SPACE:
-                    ammo.append([player.x + player.size // 2 - 2, player.y])
+                    player.ammo.append([player.x + player.size // 2 - 2, player.y])
         
-        if not boss_fight:
-            spawn_enemy()
+        if not game.boss_fight:
+            game.spawn_enemy(player, boss, display)
         else:
-            spawn_boss()
+            game.spawn_boss(boss, player, display)
         
         
         updateScreen()
         player.update_buffs()
         player.move()
-        spawn_enemies()
+        game.spawn_enemies(player)
         clock.tick(60)
     pygame.quit()
     quit()
-game()
+main()
